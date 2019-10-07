7470336ffcfc62777ad4f56be3b94ed1b449f9ac
Alan Liu <alan.y.liu@gmail.com>
2019-10-07 13:53:31 -0400

    fix typo

--- app/app/settings.py
+++ app/app/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backend.postgresl',
+        'ENGINE': 'django.db.backend.postgresql',
         'HOST': os.environ.get('DB_HOST'),
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
